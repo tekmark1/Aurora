08b1a76ba533cb28cf294bef14fb24c6cc620620
Travis Ekmark <travis@bond.co>
2018-01-15 20:45:27 -0600

    fixed display alignment

--- app/src/components/Display.js
+++ app/src/components/Display.js
@@ -8,11 +8,11 @@ class Display extends Component {
 			<div class="row display-pad">
 				<div class="col-1"></div>
 				<div class="col-10">
-					<div class="row">
-						<div class="col-4">
+					<div class="row left-align">
+						<div class="col-5">
 							<DisplayImage />
 						</div>
-						<div class="col-8">
+						<div class="col-7">
 							<DisplayText />
 						</div>
 					</div>
--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -7,7 +7,7 @@ class NavBar extends Component {
 			<div class="row navigation">
 				<div class="col-1"></div>
 				<div class="col-10 nav-bar">
-					<div class="row">
+					<div class="row align-middle">
 				{/* Add props to NavBarItem which take column-# and title*/}
 						<div class="col-2 left-pad text-center">
 							<NavBarItem />
